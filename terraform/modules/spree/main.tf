87933ac992cc8946373f3a5c9f606a0819ba2209
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2021-06-01 13:24:24 +0100

    SINF-406 - set publicly_accessible to false

--- terraform/modules/spree/main.tf
+++ terraform/modules/spree/main.tf
@@ -118,7 +118,7 @@ resource "aws_rds_cluster_instance" "cluster_instances" {
   instance_class       = var.db_instance_class
   engine               = "aurora-postgresql"
   apply_immediately    = true
-  publicly_accessible  = true
+  publicly_accessible  = false
   db_subnet_group_name = aws_db_subnet_group.spree.name
 }
 
